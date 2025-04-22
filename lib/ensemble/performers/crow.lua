aa29c7dd58ec88481de762e1fc4877b257950779
Casey Childers <cachilders@gmail.com>
2025-04-23 00:40:47 +0100

    [FEAT] Crow fix

--- lib/ensemble/performers/crow.lua
+++ lib/ensemble/performers/crow.lua
@@ -18,18 +18,26 @@ function CrowPerformer:init()
 end
 
 function CrowPerformer:play_note(sequence, note, velocity, envelope_duration)
+  local peak_env_volts = velocity * 10 /127
   local device = params:get('marco_performer_crow_device_'..sequence)
   local output = params:get('marco_performer_crow_outputs_'..sequence)
   local env_out = output == 1 and 1 or 3
   local cv_out = output == 1 and 2 or 4
   local atk, dec, sus, rel = params:get('marco_attack_'..sequence), params:get('marco_decay_'..sequence), params:get('marco_sustain_'..sequence), params:get('marco_release_'..sequence)
   local sus_period = envelope_duration - (envelope_duration * (atk + dec + rel) / 100)
-  sus = sus * (velocity / 100)
+  sus = sus * peak_env_volts / 100
   sus_period = sus_period >= 0 and sus_period or 0
-  local envelope = '{ to(0,0), to('..envelope_duration * (atk / 100)..','..velocity..'), to('..envelope_duration * (dec / 100)..','..sus..'), to('..envelope_duration * (sus_period / 100)..','..sus..'), to('..envelope_duration * (rel / 100)..',0) }'
-  note = (note - params:get('marco_root'))/12
+  envelope = string.format("{ to(0,0), to(%f,%f), to(%f,%f), to(%f,%f), to(0,%f) }",
+    peak_env_volts,
+    envelope_duration * (atk / 100),
+    sus,
+    envelope_duration * (dec / 100),
+    sus,
+    envelope_duration * (sus_period / 100),
+    envelope_duration * (rel / 100))
+  note = note / 12
   if device == 1 then
-    print(envelope)
+    print(note, envelope)
     crow.output[cv_out].volts = note
     crow.output[env_out].action = envelope
     crow.output[env_out]()
@@ -37,7 +45,7 @@ function CrowPerformer:play_note(sequence, note, velocity, envelope_duration)
     clock.run(
       function()
         -- set slew or whatever else we can...ar, for instance
-        crow.ii.crow[device - 1].volts(env_out, 5)
+        crow.ii.crow[device - 1].volts(env_out, 10)
         crow.ii.crow[device - 1].volts(cv_out, note)
         clock.sleep(envelope_duration)
         crow.ii.crow[device - 1].volts(env_out, 0)
