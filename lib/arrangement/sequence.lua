48d4e9321587713ceb1afa44f3b931a278568480
Casey A. Childers <cachilders@gmail.com>
2025-05-02 23:06:33 -0700

    Revert "[WIP] test dual transmission"
    This reverts commit 9434fdae94e06bbfed7be07720dc3152b0eb03eb.

--- lib/arrangement/sequence.lua
+++ lib/arrangement/sequence.lua
@@ -197,16 +197,20 @@ function Sequence:step_state()
 end
 
 function Sequence:transmit()
+  local id = self.id
+  local mode = get_current_mode()
   default_mode_timeout_extend()
   if mode ~= DEFAULT then
-    local sequence_states = {}
+    -- Why not both; this is where it's happening. the polymorphism is breaking the reactive sequence broadcast.
+    -- refactor to transmit both sequence and step data and allow receiver to key on mode if preferred
     local values, ranges, types 
-    values, ranges, types = self:state()
-    sequence_states[SEQUENCE] = {values, ranges, types}
-    values, ranges, types = self:step_state()
-    sequence_states[STEP] = {values, ranges, types}
-    self.transmit_editor_state(SEQUENCE, self.id, sequence_states)
-    self.transmit_editor_state(STEP, self.selected_step, sequence_states)
+    if mode == SEQUENCE then
+      values, ranges, types = self:state()
+    elseif mode == STEP then
+      id = self.selected_step
+      values, ranges, types = self:step_state()
+    end
+    self.transmit_editor_state(mode, id, {values, ranges, types})
   end
 end
 
